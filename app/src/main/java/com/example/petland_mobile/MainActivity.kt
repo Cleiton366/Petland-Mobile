c940ddc7e2409a7db2470e31cd091c0f82302475
Cleiton366 <cleiton0050cf@gmail.com>
2022-09-28 13:35:32 -0300

    Updated page view
    Updated to scroll view

--- app/src/main/java/com/example/petland_mobile/MainActivity.kt
+++ app/src/main/java/com/example/petland_mobile/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.petland_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
