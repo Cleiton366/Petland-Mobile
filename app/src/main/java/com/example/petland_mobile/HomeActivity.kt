8953e03a08c80d1459111494005bb58e05a1bf3d
Cleiton366 <cleiton0050cf@gmail.com>
2022-09-30 14:17:15 -0300

    Finished implementation between MainActivity and HomeActivity
    Rendering user photo on HomeActivity
    Fixed style of HomeActivity

--- app/src/main/java/com/example/petland_mobile/HomeActivity.kt
+++ app/src/main/java/com/example/petland_mobile/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.petland_mobile
 
+import android.net.Uri
 import com.example.petland_mobile.models.User
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.view.SimpleDraweeView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var user : User
     override fun onCreate(savedInstanceState: Bundle?) {
+
         supportActionBar?.hide()
 
         //getting user info
@@ -16,6 +22,13 @@ class HomeActivity : AppCompatActivity() {
         }
 
         super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
         setContentView(R.layout.activity_home)
+        loadUserImg(user.avatarurl)
+    }
+
+    private fun loadUserImg(url : String) {
+        val imageView = findViewById<SimpleDraweeView>(R.id.profile_image)
+        imageView.setImageURI(Uri.parse(url))
     }
 }
--- app/src/main/java/com/example/petland_mobile/MainActivity.kt
+++ app/src/main/java/com/example/petland_mobile/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.example.petland_mobile.models.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
