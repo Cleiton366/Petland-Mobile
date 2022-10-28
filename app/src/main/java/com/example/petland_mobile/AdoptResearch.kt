17df906a96113612f6420df28c34f9d5e0632618
viktorsoares <js709406@gmail.com>
2022-10-28 14:17:43 -0300

    fix bugs

--- app/src/main/java/com/example/petland_mobile/AdoptResearch.kt
+++ app/src/main/java/com/example/petland_mobile/AdoptResearch.kt
@@ -7,6 +7,7 @@ import com.example.petland_mobile.models.*
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import io.ktor.client.*
 import io.ktor.client.call.*
 import kotlin.collections.ArrayList
@@ -26,14 +27,25 @@ class AdoptResearch : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adopt_research)
 
-        fetchPetList ()
-        allPetsListView ()
+        fetchPetList()
+        allPetsListView()
+
+        /*val listView : ListView = findViewById(R.id.allPetsListView)
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val pets = petList[position]
+
+            val petsObj = petsObj(petList, pets, position)
+
+            val intent = Intent(this, Pet::class.java)
+            intent.putExtra("pet", petsObj)
+            startActivity(intent)
+        }*/
+
     }
-}
 
     private fun fetchPetList () {
         runBlocking {
-            var url = "http://192.168.0.6:4000/pet/"
+            var url = "http://192.168.0.6:4000/pet"
             val client = HttpClient(CIO) {
                 install(ContentNegotiation) {
                     gson()
@@ -47,9 +59,11 @@ class AdoptResearch : AppCompatActivity() {
         }
     }
 
-private fun allPetsListView () {
-    val listView : ListView = findViewById(R.id.allPetsListView)
-    val pets = petList.map { it. }
-    val arrayAdapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, pets)
-    listView.adapter = arrayAdapter
+    private fun allPetsListView(){
+        val listView : ListView = findViewById(R.id.allPetsListView)
+        val pets = petList.map{it}
+        val arrayAdapter : ArrayAdapter<Pet> = ArrayAdapter(this, R.layout.activity_adopt_research, pets)
+        listView.adapter = arrayAdapter
+    }
+
 }
